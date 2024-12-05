1422f41767e14e596b1329590f4768f4c63e9986
JSPeeter <xjanoskop@stuba.sk>
2024-12-05 14:25:06 +0200

    Add files via upload

---
+++ JavaFX/FrontEnd/src/main/java/org/example/frontend/MainController.java
@@ -0,0 +1,18 @@
+package org.example.frontend;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+    @FXML
+    private void onBooksClick() {
+        System.out.println("Books button clicked!");
+        // Implement logic to show Books
+    }
+
+    @FXML
+    private void onMyBooksClick() {
+        System.out.println("My Books button clicked!");
+        // Implement logic to show My Books
+    }
+
+}
